16e9bc0d7e137d64bc376813d5c7a73dea8a0106
Michael-K-M <57261773+Michael-K-M@users.noreply.github.com>
2024-01-08 21:12:50 +0200

    user safety for creating an order

--- Rural Route/adminCreateDelivery.xaml.cs
+++ Rural Route/adminCreateDelivery.xaml.cs
@@ -38,71 +38,90 @@ public partial class adminCreateDelivery : ContentPage
     private void Button_Pressed(object sender, EventArgs e)
     {
         Order order = new Order();
-        if (order.Location == null)
+
+        if (picker.SelectedItem == null)
         {
-            order.CustomerId = (picker.SelectedItem as Customer).Id;
-            order.OrderStatus = "Pending";
-            order.Location = "";
-            order.DateTime = Date_Picker.Date;
-
-            List<OrderProduct> orderProducts = new List<OrderProduct>();
-
-            foreach (var row in GridDisplay.Children)
-            {
-                var gridRow = (DataGridRow)row;
-                if (gridRow.IsFilledEntry())
-                {
-                    OrderProduct orderProduct = new OrderProduct();
-
-                    var detail = gridRow.GetGridInfo();
-                    orderProduct.ProductId = detail.productId;
-                    orderProduct.Quantity = detail.amount;
-                    orderProducts.Add(orderProduct);
-                }
-            }
-
-            App.RuralRouteRepository.CreateOrder(order, orderProducts);
-
-            picker.SelectedItem = null;
-            txt_customerAddress.Text = "";
-            GridDisplay.Clear();
-            PopulateGridView();
-
-            DisplayAlert("Great News!", "You have successfully created a new Order", "OK");
+            DisplayAlert("***BAD NEWS***", "You need to choose the company", "OK");
         }
-        else
-        {
 
-            order.CustomerId = (picker.SelectedItem as Customer).Id;
-            order.OrderStatus = "Pending";
-            order.Location = txt_customerAddress.Text;
-            order.DateTime = Date_Picker.Date;
+        else {
 
-            List<OrderProduct> orderProducts = new List<OrderProduct>();
-
-            foreach (var row in GridDisplay.Children)
+            if (order.Location == null)
             {
-                var gridRow = (DataGridRow)row;
-                if (gridRow.IsFilledEntry())
+                order.CustomerId = (picker.SelectedItem as Customer).Id;
+                order.OrderStatus = "Pending";
+                order.Location = "";
+                order.DateTime = Date_Picker.Date;
+
+                List<OrderProduct> orderProducts = new List<OrderProduct>();
+
+                foreach (var row in GridDisplay.Children)
                 {
-                    OrderProduct orderProduct = new OrderProduct();
+                    var gridRow = (DataGridRow)row;
+                    if (gridRow.IsFilledEntry())
+                    {
+                        OrderProduct orderProduct = new OrderProduct();
 
-                    var detail = gridRow.GetGridInfo();
-                    orderProduct.ProductId = detail.productId;
-                    orderProduct.Quantity = detail.amount;
-                    orderProducts.Add(orderProduct);
+                        var detail = gridRow.GetGridInfo();
+                        orderProduct.ProductId = detail.productId;
+                        orderProduct.Quantity = detail.amount;
+                        orderProducts.Add(orderProduct);
+                    }
                 }
+
+                if (orderProducts.Any())
+                {
+                    App.RuralRouteRepository.CreateOrder(order, orderProducts);
+
+                    picker.SelectedItem = null;
+                    txt_customerAddress.Text = "";
+                    GridDisplay.Clear();
+                    PopulateGridView();
+
+                    DisplayAlert("Great News!", "You have successfully created a new Order", "OK");
+                    
+                }
+                else
+                {
+                    DisplayAlert("***BAD NEWS***", "You need enter a product and/or quantity", "OK");
+                }
+
+                
             }
+            else
+            {
 
-            App.RuralRouteRepository.CreateOrder(order, orderProducts);
+                order.CustomerId = (picker.SelectedItem as Customer).Id;
+                order.OrderStatus = "Pending";
+                order.Location = txt_customerAddress.Text;
+                order.DateTime = Date_Picker.Date;
 
-            picker.SelectedItem = null;
-            txt_customerAddress.Text = "";
-            GridDisplay.Clear();
-            PopulateGridView();
+                List<OrderProduct> orderProducts = new List<OrderProduct>();
 
-            DisplayAlert("Great News!", "You have successfully created a new Order", "OK");
+                foreach (var row in GridDisplay.Children)
+                {
+                    var gridRow = (DataGridRow)row;
+                    if (gridRow.IsFilledEntry())
+                    {
+                        OrderProduct orderProduct = new OrderProduct();
 
+                        var detail = gridRow.GetGridInfo();
+                        orderProduct.ProductId = detail.productId;
+                        orderProduct.Quantity = detail.amount;
+                        orderProducts.Add(orderProduct);
+                    }
+                }
+
+                App.RuralRouteRepository.CreateOrder(order, orderProducts);
+
+                picker.SelectedItem = null;
+                txt_customerAddress.Text = "";
+                GridDisplay.Clear();
+                PopulateGridView();
+
+                DisplayAlert("Great News!", "You have successfully created a new Order", "OK");
+
+            }
         }
     }
 }
